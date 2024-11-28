5239e10620ebbc7bdb8ef3d3ac8816f8e7735fc4
Eve56784 <eva.sytikk@mail.ru>
2024-11-28 13:08:35 +0300

    1

--- src/lesson3HW/Task1.java
+++ src/lesson3HW/Task1.java
@@ -1,9 +1,9 @@
 package lesson3HW;
 
-public class task1 {
+public class Task1 {
     public static void main(String[] args){
-        int b=3;
-        float c=2.25f;
+        int b = 3;
+        float c = 2.25f;
         float a = 4.0f * (b + c - 1) / 2.0f;
         System.out.println("\tThe value of a is: "+a);
     }
---
+++ src/lesson3HW/Task2.java
@@ -0,0 +1,9 @@
+package lesson3HW;
+
+public class Task2 {
+    public static void main(String[] args){
+        int n = 69;
+        int sum = n%10 + (n/10);
+        System.out.println("Sum is: "+sum);
+    }
+}

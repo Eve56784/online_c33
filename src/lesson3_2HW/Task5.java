9fdfd93b3d33c5f5bbb099ca28505854cd94406d
Eve56784 <eva.sytikk@mail.ru>
2024-11-28 15:18:40 +0300

    1

--- src/lesson3_2HW/Task5.java
+++ src/lesson3_2HW/Task5.java
@@ -1,7 +1,24 @@
 package lesson3_2HW;
 
+import java.util.Scanner;
+
 public class Task5 {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int i = 1;
+        int num = 0;
 
+        while (num <= 0) {
+            System.out.print("Enter positive integer: ");
+            num = scanner.nextInt();
+            if (num <= 0) {
+                System.out.println("Input error. Please enter a positive integer.");
+            }
+        }
+
+        while (i < num) {
+            System.out.print(i + " ");
+            i++;
+        }
     }
 }

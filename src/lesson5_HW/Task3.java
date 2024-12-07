03ed23bdb8c33e797a05962860b76a86be7d747a
Eve56784 <eva.sytikk@mail.ru>
2024-12-07 13:35:24 +0300

    /

--- src/lesson5_HW/Task3.java
+++ src/lesson5_HW/Task3.java
@@ -1,4 +1,23 @@
 package lesson5_HW;
 
+import java.util.Scanner;
+
 public class Task3 {
+    public static void main(String[] args) {
+        inputScan();
+        snakeArrayCreation();
+        output();
+    }
+
+    public static void inputScan() {
+
+    }
+
+    public static void snakeArrayCreation() {
+
+    }
+
+    public static void output() {
+
+    }
 }

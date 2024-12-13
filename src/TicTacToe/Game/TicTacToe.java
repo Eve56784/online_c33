6ab187160582e092ba60d163c28032193196b0cd
Eve56784 <eva.sytikk@mail.ru>
2024-12-13 22:08:26 +0300

    /

--- src/TicTacToe/Game/TicTacToe.java
+++ src/TicTacToe/Game/TicTacToe.java
@@ -8,54 +8,80 @@ public class TicTacToe {
         startGame();
     }
 
-    public static void startGame(){
-        getInitialMessage();
+    public static void startGame() {
+        boolean newbie = true;
+        Scanner scanner = getInputValue();
+        while (true) {
+            if (newbie) {
+                getInitialMessage();
+            }
+            if (scanner.nextLine().equals("y")) {
+                newbie = false;
+                boolean win = false;
+                System.out.println("✄┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈");
+                char[][] gameTable = createInitializeTable();
+                drawTable(gameTable);
 
-        if((getInputValue().nextLine()).equals("y")){
-            System.out.println("-ˋˏ✄┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈");
-            char[][] gameTable = createInitializeTable();
-            drawTable(gameTable);
-
-            int dice = getRandomNumbers().nextInt(2);
-            if(dice==0)
-                makeAIMove(gameTable, 'X');
-            else
-                makePlayerMove(gameTable, 'X');
-
-            int moveCount = 1;
-            while(moveCount<9){
-                if(dice==0){
+                int dice = getRandomNumbers().nextInt(2);
+                if (dice == 0) {
                     makeAIMove(gameTable, 'X');
-                    moveCount++;
-                    if(checkGameStatus(gameTable, 'X')) {
-                        System.out.println("AI has won... (¬`‸´¬)\nDo you wanna take revenge? (y/n): ");
-                        return;
-                    }
-                    makePlayerMove(gameTable, 'O');
-                    moveCount++;
-                    if(checkGameStatus(gameTable, 'O')) {
-                        System.out.println("You have won!! ദി(˵ •̀ ᴗ - ˵ ) ✧");
-                        return;
-                    }else{
-                        makePlayerMove(gameTable, 'X');
+                } else {
+                    makePlayerMove(gameTable, 'X');
+                }
+
+                int moveCount = 1;
+                while (moveCount < 9) {
+                    if (dice == 0) {
+                        makePlayerMove(gameTable, 'O');
                         moveCount++;
-                        if(checkGameStatus(gameTable, 'X')) {
-                            System.out.println("You have won!! ദി(˵ •̀ ᴗ - ˵ ) ✧");
-                            return;
+                        if (checkGameStatus(gameTable, 'O')) {
+                            System.out.println("\u001b[32;1mYou have won!! ദി(˵ •̀ ᴗ - ˵ ) " +
+                                    "✧\nDo you wanna play again? (y/n): \u001b[0m");
+                            win = true;
+                            break;
                         }
+                        makeAIMove(gameTable, 'X');
+                        moveCount++;
+                        if (checkGameStatus(gameTable, 'X')) {
+                            System.out.println("\u001b[34;1mAI has won... (¬`‸´¬)" +
+                                    "\nDo you wanna take revenge? (y/n): \u001b[0m");
+                            win = true;
+                            break;
+                        }
+                    } else {
                         makeAIMove(gameTable, 'O');
                         moveCount++;
-                        if(checkGameStatus(gameTable, 'O')) {
-                            System.out.println("AI has won... (¬`‸´¬)\nDo you wanna take revenge? (y/n): ");
-                            return;
+                        if (checkGameStatus(gameTable, 'O')) {
+                            System.out.println("\u001b[34;1mAI has won... (¬`‸´¬)" +
+                                    "\nDo you wanna take revenge? (y/n): \u001b[0m");
+                            win = true;
+                            break;
+                        }
+                        makePlayerMove(gameTable, 'X');
+                        moveCount++;
+                        if (checkGameStatus(gameTable, 'X')) {
+                            System.out.println("\\u001b[32;1mYou have won!! ദി(˵ •̀ ᴗ - ˵ ) \" +\n" +
+                                    "\"✧\\nDo you wanna play again? (y/n): \\u001b[0m");
+                            win = true;
+                            break;
                         }
                     }
                 }
-                System.out.println("It's a DRAW. Would you like to play again? (y/n): ");
+                if (moveCount == 9 && !win) {
+                    System.out.println("\u001b[33;1mIt's a DRAW. Would you like to play again? (y/n): \u001b[0m");
+                }
+            } else {
+                System.out.println("See ya!");
+                break;
             }
-        }else
-            System.out.println("See ya!");
+
+            if (!scanner.nextLine().equals("y")) {
+                System.out.println("See ya!");
+                break;
+            }
+        }
     }
+
 
     public static void getInitialMessage(){
         System.out.print("\u001b[47m\u001b[30m");
@@ -84,13 +110,14 @@ public class TicTacToe {
     public static void makeAIMove(char[][] gameTable, char symbol){
         int rowAI, columnAI;
         while(true){
-            rowAI = getRandomNumbers().nextInt(2);
-            columnAI = getRandomNumbers().nextInt(2);
+            rowAI = getRandomNumbers().nextInt(3);
+            columnAI = getRandomNumbers().nextInt(3);
             if(gameTable[rowAI][columnAI]=='*'){
                 gameTable[rowAI][columnAI]=symbol;
                 break;
             }
         }
+        System.out.println("✄┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈");
         System.out.println("AI has decided to make the following move: [" + (rowAI+1) + "]["
                 + (columnAI+1) + "]");
         drawTable(gameTable);
@@ -98,18 +125,21 @@ public class TicTacToe {
     public static void makePlayerMove(char[][] gameTable, char symbol){
         Scanner scanner = getInputValue();
         int rowPlayer, columnPlayer;
+        System.out.println("✄┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈");
         while(true){
             System.out.println("Enter the row number (1-3): ");
             rowPlayer = scanner.nextInt()-1;
             System.out.println("Enter the column number (1-3): ");
             columnPlayer = scanner.nextInt()-1;
 
-            if(rowPlayer>0 && rowPlayer <= 3 && columnPlayer >0 && columnPlayer <=3){
-                gameTable[rowPlayer][columnPlayer] = symbol;
-                System.out.println("You have decided to make the following move: [" + (rowPlayer+1) + "]["
-                        + (columnPlayer+1) + "]");
-                drawTable(gameTable);
-                break;
+            if(rowPlayer>=0 && rowPlayer <= 2 && columnPlayer >=0 && columnPlayer <=2) {
+                if (gameTable[rowPlayer][columnPlayer] == '*') {
+                    gameTable[rowPlayer][columnPlayer] = symbol;
+                    System.out.println("You have decided to make the following move: [" + (rowPlayer + 1) + "]["
+                            + (columnPlayer + 1) + "]");
+                    drawTable(gameTable);
+                    break;
+                }
             }
             System.out.println("\u001b[43m\u001b[30mYou have entered the wrong value...\u001b[0m");
         }

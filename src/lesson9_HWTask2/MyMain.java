79e3bf63f7a9d36e0946b45d89d5bf203a26701d
Eve56784 <eva.sytikk@mail.ru>
2024-12-22 12:02:17 +0300

    /

--- src/lesson9_HWTask2/MyMain.java
+++ src/lesson9_HWTask2/MyMain.java
@@ -1,17 +1,16 @@
 package lesson9_HWTask2;
 
 public class MyMain {
-    private static Figure[] figures;
     public static void main(String[] args) {
-        figures = new Figure[]{
+        Figure[] figures = new Figure[]{
                 new Circle(3),
                 new Rectangle(3.3, 10),
                 new Triangle(5, 12, 13),
                 new Circle(9),
-                new Triangle(3, 7.6, 10.5)
+                new Triangle(3, 8.3, 10.5)
         };
         double totalPerimeter = 0, totalArea = 0;
-        for(Figure figure:figures){
+        for(Figure figure: figures){
             totalPerimeter+=figure.findThePerimeter();
             totalArea+=figure.findTheArea();
         }

33e5ec6228a4c8b89dc2660e47c28560abf3d8d4
Eve56784 <eva.sytikk@mail.ru>
2025-01-25 13:16:20 +0300

    hw12

---
+++ src/lesson12_HW/Task1.java
@@ -0,0 +1,18 @@
+package lesson12_HW;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Enter string: ");
+        String str = new Scanner(System.in).nextLine();
+        String[] words = str.split(" ");
+        for (String word : words){
+            if(isAbbreviation(word))
+                System.out.println(word);
+        }
+    }
+    public static boolean isAbbreviation(String word){
+        return word.length() >= 2 && word.length() < 6 && word.matches("[A-Z]+");
+    }
+}

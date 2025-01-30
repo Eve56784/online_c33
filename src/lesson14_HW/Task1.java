bb5fbb8e5a90f6c5e5f67d19aeac5f34eb843666
Eve56784 <eva.sytikk@mail.ru>
2025-01-30 16:32:40 +0300

    .

---
+++ src/lesson14_HW/Task1.java
@@ -0,0 +1,29 @@
+package lesson14_HW;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Task1 {
+    public static void main(String[] args) {
+        String output = "C:\\Users\\mayia\\IdeaProjects\\project123\\src\\lesson14_HW\\TextForTask1";
+        String input = "C:\\Users\\mayia\\IdeaProjects\\project123\\src\\lesson14_HW\\NewFileForTask1";
+        String theLongestWord = "";
+        try(Scanner scanner = new Scanner(new File(output))){
+            while(scanner.hasNext()){
+                String word = scanner.next();
+                if(word.length()>theLongestWord.length())
+                    theLongestWord = word;
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("Can't open the file: "+ e.getMessage());
+        }
+        try(FileWriter writer = new FileWriter(input)){
+            writer.append(theLongestWord);
+        }catch(IOException e){
+            System.out.println("Can't open the file: "+ e.getMessage());
+        }
+    }
+}

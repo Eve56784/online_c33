68a214dd9d3d7f02ec7ce733dc216265cd5cbc22
Eve56784 <eva.sytikk@mail.ru>
2025-03-07 20:03:32 +0300

    -

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static String createString(){
+        return "Operation status: Success";
+    }
+    public static void main( String[] args )
+    {
+        System.out.println(createString());
+    }
+}
+/*java -jar target/ProjectTMS-1.0-SNAPSHOT.jar*/

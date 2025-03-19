bb8ed454776073bf5c612273ae08fe3dd555e777
Yiwa Taitzi <lublumariny228@mail.ru>
2025-03-19 16:09:31 +0300

    Delete src/lesson3_2HW directory

--- src/lesson3_2HW/Task3.java
+++
@@ -1,9 +0,0 @@
-package lesson3_2HW;
-
-public class Task3 {
-    public static void main(String[] args){
-        for (int i=10; i>=10 && i<=20; i++){
-            System.out.println(i*i);
-        }
-    }
-}

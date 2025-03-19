858c4d539f00c6554945f9486e54f8646ac36e4b
Yiwa Taitzi <lublumariny228@mail.ru>
2025-03-19 16:09:38 +0300

    Delete src/lesson3_1HW directory

--- src/lesson3_1HW/Task2.java
+++
@@ -1,9 +0,0 @@
-package lesson3_1HW;
-
-public class Task2 {
-    public static void main(String[] args){
-        int n = 69;
-        int sum = n%10 + (n/10);
-        System.out.println("Sum is: "+sum);
-    }
-}

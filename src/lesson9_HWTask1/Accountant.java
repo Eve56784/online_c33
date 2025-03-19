a59c394de3dac8c014968fe72888643a4d32027f
Yiwa Taitzi <lublumariny228@mail.ru>
2025-03-19 16:11:04 +0300

    Delete src/lesson9_HWTask1 directory

--- src/lesson9_HWTask1/Accountant.java
+++
@@ -1,8 +0,0 @@
-package lesson9_HWTask1;
-
-public class Accountant implements InterfaceTask{
-    @Override
-    public void print() {
-        System.out.println("\033[1;34mI'm an accountant...\033[1;0m");
-    }
-}

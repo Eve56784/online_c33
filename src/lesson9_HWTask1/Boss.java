18a152fa91659125dc591144137c669ed90cd153
Eve56784 <eva.sytikk@mail.ru>
2025-03-19 16:11:40 +0300

    .

--- src/lesson9_HWTask1/Boss.java
+++
@@ -1,8 +0,0 @@
-package lesson9_HWTask1;
-
-public class Boss implements InterfaceTask{
-    @Override
-    public void print() {
-        System.out.println("\033[1;33mI'm a BOSS...\033[1;0m");
-    }
-}
--- src/lesson9_HWTask2/Figure.java
+++
@@ -1,6 +0,0 @@
-package lesson9_HWTask2;
-
-abstract class Figure{
-    abstract double findTheArea();
-    abstract double findThePerimeter();
-}

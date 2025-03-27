a675a219d216b0b7312755624d42d0459b4bc893
Eve56784 <eva.sytikk@mail.ru>
2025-03-27 20:25:19 +0300

    .

--- src/main/java/task2/MyServlet.java
+++ src/main/java/task2/MyServlet.java
@@ -29,7 +29,7 @@ public class MyServlet extends HttpServlet {
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-        super.init();
+        super.init(config);
         System.out.println("The app was initialized");
     }
 

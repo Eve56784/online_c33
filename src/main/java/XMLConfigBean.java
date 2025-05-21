8689557322d7b28c8de711d9acc3814f0aae65c9
Eve56784 <eva.sytikk@mail.ru>
2025-05-21 18:52:35 +0300

    hw37 Spring

--- src/main/java/XMLConfigBean.java
+++ src/main/java/XMLConfigBean.java
@@ -1,5 +1,13 @@
 public class XMLConfigBean {
+
+    public void init(){
+        System.out.println("XML config bean was initialized");
+    }
     public void greet(){
-        System.out.println("Hello");
+        System.out.println("Hello (XMLConfigBean)!");
+    }
+
+    public void preDestroy(){
+        System.out.println("XML config bean's gonna be destroyed");
     }
 }

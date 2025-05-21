205b3ee2d8cf0fa874397b083d8d5bdb6d311dd7
Eve56784 <eva.sytikk@mail.ru>
2025-05-21 18:52:17 +0300

    hw37 Spring

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,10 +1,16 @@
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyMain {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        XMLConfigBean bean = context.getBean(XMLConfigBean.class);
-        bean.greet();
+        ApplicationContext context1 = new ClassPathXmlApplicationContext("applicationContext.xml");
+        XMLConfigBean xmlBean = context1.getBean(XMLConfigBean.class);
+        ApplicationContext context2 = new AnnotationConfigApplicationContext(AppConfig.class);
+        JavaConfigBean javaBean = context2.getBean(JavaConfigBean.class);
+        xmlBean.greet();
+        javaBean.greet();
+        ((ClassPathXmlApplicationContext) context1).close();
+        ((AnnotationConfigApplicationContext) context2).close();
     }
 }

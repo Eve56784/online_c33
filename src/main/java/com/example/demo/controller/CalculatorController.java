3d63e01894f5dd5753ad59a9e474837b9921bdd4
Eve56784 <eva.sytikk@mail.ru>
2025-05-30 18:43:22 +0300

    hw41

--- src/main/java/com/example/demo/controller/CalculatorController.java
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.exception.DivisionByZeroException;
 import com.example.demo.validation.CalculationRequest;
+import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,14 +10,30 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 @Controller
 public class CalculatorController {
+    private final MeterRegistry meterRegistry;
+
+    public CalculatorController(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
 
     @GetMapping
     public String showForm(){
         return "calculator";
+    }
+
+    @GetMapping("/metrics")
+    public String showMetrics(Model model){
+        Map<String, Integer> metrics = new HashMap<>();
+        meterRegistry.find("calculator.operations").counters().forEach(counter -> metrics
+                .put(counter.getId().getTag("type"), (int) counter.count()));
+        model.addAttribute("metrics", metrics);
+        return "metrics";
     }
 
     @PostMapping("/calculate")
@@ -33,10 +50,22 @@ public class CalculatorController {
         double result;
 
         switch (request.getOperation()) {
-            case "add": result = request.getA() + request.getB(); break;
-            case "subtract": result = request.getA() - request.getB(); break;
-            case "multiply": result = request.getA() * request.getB(); break;
-            case "divide": result = request.getA() / request.getB(); break;
+            case "add":
+                result = request.getA() + request.getB();
+                meterRegistry.counter("calculator.operations", "type", "add").increment();
+                break;
+            case "subtract":
+                result = request.getA() - request.getB();
+                meterRegistry.counter("calculator.operations", "type", "sub").increment();
+                break;
+            case "multiply":
+                result = request.getA() * request.getB();
+                meterRegistry.counter("calculator.operations", "type", "mul").increment();
+                break;
+            case "divide":
+                result = request.getA() / request.getB();
+                meterRegistry.counter("calculator.operations", "type", "div").increment();
+                break;
             default:
                 model.addAttribute("error", "Invalid operation");
                 return "calculator";
